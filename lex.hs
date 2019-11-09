8ae546df00869f35a876c93b1ca739def6d52129
Evian-Zhang <evianzhang1999@163.com>
2019-11-10 01:22:37 +0800

    complete regular tokens to nfa

--- lex.hs
+++ lex.hs
@@ -45,22 +45,38 @@ shunting_yard x = shunting_yard' x [] [] where
     shunting_yard' xs@(x:remain) s q = case x of
         REChar c -> shunting_yard' remain s (q ++ [REChar c])
         REOperator operator ->
-            if s /= [] && is_operator (head s) && priority (head s) > priority (REOperator operator)
+            if not (null s) && is_operator (head s) && priority (head s) > priority (REOperator operator)
                 then shunting_yard' xs (tail s) (q ++ [head s])
                 else shunting_yard' remain (REOperator operator : s) q
         ParenOpen -> shunting_yard' remain (ParenOpen : s) q
         ParenClose ->
-            if s /= [] && head s /= ParenOpen
+            if not (null s) && head s /= ParenOpen
                 then shunting_yard' xs (tail s) (q ++ [head s])
                 else shunting_yard' remain (tail s) q
 
 data State = State Int deriving (Eq, Ord, Show)
+
+shift_state :: Int->State->State
+shift_state index (State x) = State (x + index)
 
 data NFA = NFA { states :: [State]
                , edges :: (State -> RECharType -> [State])
                , start_state :: State
                , end_state :: State
                }
+
+shift_NFA :: Int -> NFA -> NFA
+shift_NFA index nfa = 
+    NFA { states = states'
+        , edges = edges'
+        , start_state = start_state'
+        , end_state = end_state'
+        }
+    where
+        start_state' = shift_state index $ start_state nfa
+        end_state' = shift_state index $ end_state nfa
+        states' = List.map (shift_state index) (states nfa)
+        edges' = \state c -> List.map (shift_state index) $ edges nfa (shift_state (negate index) state) c
 
 char_to_NFA :: RECharType -> NFA
 char_to_NFA c = case c of
@@ -92,22 +108,95 @@ binary_operator_to_NFA operator lchild rchild = case operator of
             }
         where
             start_state' = start_state lchild
-            end_state' = end_state rchild
-            State lend_index = end_state'
-            rchild_states = states rchild
-            rchild_states' = List.map (\(State previous) -> (State (previous + lend_index + 1))) rchild_states'
+            State lend_index = end_state lchild
+            rchild' = shift_NFA (lend_index + 1) rchild
+            end_state' = end_state rchild'
+            rchild_states' = states rchild'
             states' = states lchild ++ rchild_states'
-            rchild_edges = edges rchild
-            rchild_edges' = \(State index) c -> List.map (\(State previous_index) -> (State (previous_index + lend_index + 1))) (rchild_edges (State (index + lend_index + 1)) c)
-            edges' = \(State index) c -> if index <= lend_index
+            edges' = \(State index) c -> if index < lend_index
                                             then edges lchild (State index) c
-                                            else rchild_edges' (State index) c
-    Or -> error "Unexpected error"
-    Repeat -> error "Unexpected error"
+                                            else 
+                                                if index == lend_index
+                                                    then start_state rchild' : (edges lchild (State index) c)
+                                                    else edges rchild' (State index) c
+    Or -> 
+        NFA { states = states'
+            , edges = edges'
+            , start_state = start_state'
+            , end_state = end_state'
+            }
+        where
+            start_state' = State 0
+            lchild' = shift_NFA 1 lchild
+            State lend_index = end_state lchild'
+            rchild' = shift_NFA (lend_index + 1) rchild
+            State rend_index = end_state rchild'
+            end_state' = State (rend_index + 1)
+            states' = start_state' : (end_state' : ((states lchild') ++ (states rchild')))
+            edges' = \(State index) c -> 
+                if index == 0
+                    then if c == Epsilon then [start_state lchild', start_state rchild'] else []
+                    else 
+                        if index < lend_index 
+                            then edges lchild' (State index) c
+                            else
+                                if index < rend_index
+                                    then edges rchild' (State index) c
+                                    else
+                                        if index == rend_index && c == Epsilon
+                                            then end_state' : (edges lchild' (State index) c) ++ (edges rchild' (State index) c)
+                                            else []
+    _ -> error "Unexpected error"
 
+-- unary_operator_to_NFA operator child
+-- @param operator unary operator
+-- @param child operand of operator
+unary_operator_to_NFA :: REOperatorType -> NFA -> NFA
+unary_operator_to_NFA operator child = case operator of
+    Repeat -> 
+        NFA { states = states'
+            , edges = edges'
+            , start_state = start_state'
+            , end_state = end_state'
+            }
+        where
+            start_state' = State 0
+            child' = shift_NFA 1 child
+            State end_index' = end_state child'
+            end_state' = State (end_index' + 1)
+            states' = start_state' : (end_state' : (states child'))
+            edges' = \(State index) c ->
+                if index == 0
+                    then if c == Epsilon then [start_state child'] else []
+                    else
+                        if index < end_index'
+                            then edges child' (State index) c
+                            else 
+                                if index == end_index' && c == Epsilon
+                                    then end_state' : (start_state child' : (edges child' (State index) c))
+                                    else []
+    _ -> error "Unexpected error"
 
--- regular_expression_to_NFA :: [REToken] -> NFA
--- regular_expression_to_NFA [] = NFA {states=Set.empty, edges=(\_ _ -> Set.empty), start_state=}
+regular_tokens_to_NFA :: [REToken] -> NFA
+-- regular_tokens_to_NFA' tokens s
+-- @param tokens regular tokens
+-- @param s output stack
+regular_tokens_to_NFA tokens = regular_tokens_to_NFA' tokens [] where
+    regular_tokens_to_NFA' [] [nfa] = nfa
+    regular_tokens_to_NFA' [] _ = error "Regular expression not valid!"
+    regular_tokens_to_NFA' xs@(x:remain) s = case x of
+        REChar c -> regular_tokens_to_NFA' remain (char_to_NFA c : s)
+        REOperator And -> case s of
+            (rchild : lchild : s_remain) -> regular_tokens_to_NFA' remain $ binary_operator_to_NFA And lchild rchild : s_remain
+            _ -> error "Regular expression not valid!"
+        REOperator Or -> case s of
+            (rchild : lchild : s_remain) -> regular_tokens_to_NFA' remain $ binary_operator_to_NFA Or lchild rchild : s_remain
+            _ -> error "Regular expression not valid!"
+        REOperator Repeat -> case s of 
+            (child : s_remain) -> regular_tokens_to_NFA' remain $ unary_operator_to_NFA Repeat child : s_remain
+            _ -> error "Regular expression not valid!"
+        _ -> error "Unexpected error"
+
 
 charset :: [REToken] -> [REToken]
 charset [] = []
