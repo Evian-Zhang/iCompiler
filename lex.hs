16124ab64a564f42d38791cfd013d5ca32f7823b
Evian-Zhang <evianzhang1999@163.com>
2019-11-16 10:21:54 +0800

    add scanner

--- lex.hs
+++ lex.hs
@@ -21,20 +21,23 @@ input priority handle expressions = do
 
 main = do
     args <- getArgs
-    let file_path = args !! 0
-    inputHandle <- openFile file_path ReadMode
-    expressions <- input 0 inputHandle []
-    let postfix_regular_expressions = map (\(id, regular_expression) -> (id, shunting_yard $ tokenize_regular_expression regular_expression)) expressions
-        nfa = regular_expressions_to_NFA postfix_regular_expressions
-        dfa = nfa_to_dfa nfa
-        dfao = dfa_to_dfao dfa
-    putStrLn "---------NFA---------"
-    print nfa
-    putStrLn "---------DFA---------"
-    print dfa
-    putStrLn "----minimized DFA----"
-    print dfao
-    let dfao_str = display_dfao_edges dfao
-        end_states_str = display_dfao_end_states dfao
-    writeFile (args !! 1) dfao_str
-    writeFile (args !! 2) end_states_str
+    if length args /= 3
+        then error "There must be THREE parameters."
+        else do
+            let file_path = args !! 0
+            inputHandle <- openFile file_path ReadMode
+            expressions <- input 0 inputHandle []
+            let postfix_regular_expressions = map (\(id, regular_expression) -> (id, shunting_yard $ tokenize_regular_expression regular_expression)) expressions
+                nfa = regular_expressions_to_NFA postfix_regular_expressions
+                dfa = nfa_to_dfa nfa
+                dfao = dfa_to_dfao dfa
+            putStrLn "---------NFA---------"
+            print nfa
+            putStrLn "---------DFA---------"
+            print dfa
+            putStrLn "----minimized DFA----"
+            print dfao
+            let dfao_str = display_dfao_edges dfao
+                end_states_str = display_dfao_end_states dfao
+            writeFile (args !! 1) dfao_str
+            writeFile (args !! 2) end_states_str
